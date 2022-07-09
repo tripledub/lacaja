1dedcb7f29f546070d6103cb069f7679ab8ae6f8
Stewart Campbell <stewartcuk@gmail.com>
2022-07-09 07:31:40 +0200

    Implement totaling methods
    Select rule from pricing_rules
    Calculate total in basket

--- checkout.rb
+++ checkout.rb
@@ -1,12 +1,27 @@
-class Checkout
-  attr_reader :items
+require_relative 'discount_rule'
 
-  def initialize
+class Checkout
+  attr_reader :items, :pricing_rules
+
+  def initialize(pricing_rules: [])
     @items = {}
+    @pricing_rules = pricing_rules
   end
 
   def scan(product_code:)
     items[product_code] ||= 0
     items[product_code] += 1
+  end
+
+  def total
+    items.inject(0) do |subtotal, (product_code, quantity)|
+      subtotal + rule_for(product_code:).price_for(quantity:)
+    end
+  end
+
+  private
+
+  def rule_for(product_code:)
+    pricing_rules.find { |rule| rule.handles?(product_code:) }
   end
 end
--- spec/checkout_spec.rb
+++ spec/checkout_spec.rb
@@ -1,4 +1,5 @@
 require_relative '../checkout'
+require 'pry'
 
 Product = Struct.new(:name, :product_code, :price)
 
@@ -6,8 +7,9 @@ RSpec.describe Checkout do
   let(:green_tea) { Product.new('Green Tea', 'GR1', 3.11) }
   let(:strawberries) { Product.new('Strawberries', 'SR1', 5) }
   let(:coffee) { Product.new('Coffee', 'CF1', 11.23) }
+  let(:pricing_rules) { [] }
 
-  subject { described_class.new(pricing_rules: []) }
+  subject { described_class.new(pricing_rules:) }
 
   describe 'scanning products' do
     context 'when checkout is initialized' do
@@ -48,6 +50,45 @@ RSpec.describe Checkout do
 
       it 'does not increment the Coffee count' do
         expect(subject.items[coffee.product_code]).to eq(1)
+      end
+    end
+  end
+
+  describe '#total' do
+    let(:green_tea_rule) do
+      DiscountRule.new(
+        product: green_tea,
+        rule: Rules::BuyOneGetOneFree
+      )
+    end
+
+    let(:strawberries_rule) do
+      Rules::AbsolutePrice
+    end
+
+    let(:pricing_rules) do
+      [green_tea_rule]
+    end
+
+    context 'with no items' do
+      it 'equals 0' do
+        expect(subject.total).to eq(0)
+      end
+    end
+
+    context 'with 1 Green Tea' do
+      before { subject.scan(product_code: green_tea.product_code) }
+
+      it 'equals 3.11' do
+        expect(subject.total).to eq(3.11)
+      end
+
+      context 'with 2 Green Teas' do
+        it 'still equals 3.11 (Buy 1 Get 1 Free)' do
+          subject.scan(product_code: green_tea.product_code)
+          expect(subject.items['GR1']).to eq(2)
+          expect(subject.total).to eq(3.11)
+        end
       end
     end
   end
